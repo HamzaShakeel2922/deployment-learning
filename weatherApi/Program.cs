9026065f0efe1c9820598e0a2ed6fabd79ddcf19
faa <faa@FAA_RENTAL>
2026-08-12 06:29:30 +0300

    deployment fix

--- weatherApi/Program.cs
+++ weatherApi/Program.cs
@@ -28,6 +28,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+await db.Database.MigrateAsync();
+
 app.UseCors("AllowFrontend");
 
 // Configure the HTTP request pipeline.
